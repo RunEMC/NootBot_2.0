f9271873e17433678417a7929c84c6e9727b3ee1
RunEMC <runlin.li@gmail.com>
2017-01-15 12:04:34 -0500

    Quick uodate

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
 import os
+import sys
 import discord
-'''
 import logging
 
+# Logging/Debugging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-'''
 
 client = discord.Client()
 
 # Simple client login and starting the bot.
-client.login("MTc3MTc3ODgwNjkyOTE2MjI1.C103MA.sp3U_F2IBgtf6yKCLu2x7tWET-M")
-client.run()
+login_token = ('MTc3MTc3ODgwNjkyOTE2MjI1.C103MA.sp3U_F2IBgtf6yKCLu2x7tWET-M')
+client.login()
+client.run(login_token)
