e687dc864553f4c8ad4fb6151f11af05ad249d49
Talaal Mirza <t.pineapples@gmail.com>
2017-01-15 12:03:00 -0500

    Added the echo command, fixed client login

--- bot.py
+++ bot.py
@@ -1,17 +1,24 @@
 import os
 import discord
-'''
-import logging
+import asyncio
+from discord.ext import commands
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-'''
 
-client = discord.Client()
+client = commands.Bot(command_prefix="!", description="Meme Machine")
+
+
+@client.event
+async def on_ready():
+    print("Logged in as\n"
+          "{}\n"
+          "{}\n"
+          "---".format(client.user.name, client.user.id))
+
+
+@client.command()
+async def echo():
+    await bot.say('Echo')
+
 
 # Simple client login and starting the bot.
-client.login("MTc3MTc3ODgwNjkyOTE2MjI1.C103MA.sp3U_F2IBgtf6yKCLu2x7tWET-M")
-client.run()
+client.run("MTc3MTc3ODgwNjkyOTE2MjI1.C103MA.sp3U_F2IBgtf6yKCLu2x7tWET-M")
