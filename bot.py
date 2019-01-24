363eb4ec1a43d00718dcaa5cdc75357805327e74
Ron Li <runlin.li@gmail.com>
2019-01-23 22:06:17 -0500

    Basic Commands Update - On message now wait for bot to be ready before processing messages - New command getid - Die command can now only be called by RunEMC

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import asyncio
 from discord.ext import commands
 import json
 # import sounds
-import pokemon
+# import pokemon
 
 # Logging/Debugging
 logger = logging.getLogger('discord')
@@ -18,13 +18,25 @@ logger.addHandler(handler)
 # Create new client
 client = discord.Client()
 
+# Global flag variables
+isBotReady = False
+
+# Handle on ready events
+@client.event
+async def on_ready():
+    global isBotReady
+    isBotReady = True
+    print("Bot ready!")
+
+# Handle on message events
 @client.event
 async def on_message(message):
     msgContents = message.content
     msgChannel = message.channel
+    msgAuthor = message.author
 
     # Ensure that the message is using Pybot Commands
-    if msgContents.startswith('!'):
+    if msgContents.startswith('!') and isBotReady:
         # Get command
         commandArray = msgContents.split(" ")
         command = commandArray[0][1:]
@@ -35,11 +47,22 @@ async def on_message(message):
             await client.send_message(msgChannel, "I can't help you right now.")
 
         elif command == "die":
-            await client.send_message(msgChannel, "Ok, goodbye!")
-            await client.logout()
+            if msgAuthor.id == "171429655008509954": # You can set this to your own id
+                await client.send_message(msgChannel, "Ok, goodbye!")
+                await client.logout()
+            else:
+                await client.send_message(msgChannel, "Sorry, only RunEMC can do that!")
 
+        elif command == "getid":
+            await client.send_message(msgChannel, msgAuthor.id)
+
+        # Default
         else:
+            print(msgAuthor.id)
             await client.send_message(msgChannel, "Unrecognized command: " + msgContents)
+
+    elif not isBotReady:
+        await client.send_message(msgChannel, "Sorry, I'm still starting up, please wait")
 
 
 # Logs in the client and runs the bot using token read in from config.json
